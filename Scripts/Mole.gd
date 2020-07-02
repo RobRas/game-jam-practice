e7e3afa1934ca497a6af37640e6bc602c303054a
Robert <bobbyras90@gmail.com>
2020-07-01 20:33:45 -0700

    Can click to select different moles

--- Scripts/Mole.gd
+++ Scripts/Mole.gd
@@ -1,5 +1,8 @@
 extends KinematicBody2D
 
+
+signal clicked(mole)
+
 export(int) var move_speed = 12000
 export(int) var jump_speed = -14000
 export(int) var gravity = 20000
@@ -12,17 +15,31 @@ var facing_right = true
 
 
 var enabled = false
+
+var saved_horizontal = 0.0
+
+
+var has_mouse = false
 	
 	
 
+func _input(event):
+	if has_mouse && event.is_action_pressed("select_mole"):
+		emit_signal("clicked", self)
+
 func _physics_process(delta):
 	_set_grounding()
-	_parse_horizontal_input()
 	
-	if Input.is_action_just_pressed("jump") && is_grounded:
-		jump()
+	if enabled:
+		_parse_horizontal_input()
+		if Input.is_action_just_pressed("jump") && is_grounded:
+			jump()
 	
-	velocity.y += gravity * delta   #times delta twice due to acceleration
+	else:
+		if is_grounded:
+			velocity.x = 0
+	
+	_apply_gravity(delta)
 
 	var motion = velocity * delta
 	move_and_slide(motion)
@@ -58,8 +75,21 @@ func _parse_horizontal_input():
 		horizontal += move_speed
 	
 	velocity.x = horizontal
+	saved_horizontal = horizontal
 	
 	if horizontal == 0 or not is_grounded:
 		$Audio/RunAudio.stop()
 	elif not $Audio/RunAudio.playing:
 		$Audio/RunAudio.play()
+
+func _apply_gravity(delta):
+	if not is_grounded:
+		velocity.y += gravity * delta   #times delta twice due to acceleration
+
+
+func _on_Mole_mouse_entered():
+	has_mouse = true
+
+
+func _on_Mole_mouse_exited():
+	has_mouse = false
--- Scripts/MoleController.gd
+++ Scripts/MoleController.gd
@@ -6,9 +6,20 @@ var current_mole = null
 
 func _ready():
 	moles = get_tree().get_nodes_in_group("Moles")
+	
+	for mole in moles:
+		mole.connect("clicked", self, "_on_mole_clicked")
+	
 	set_current_mole(moles[0])
 
 func set_current_mole(mole):
-	current_mole.enabled = false
+	if current_mole:
+		current_mole.enabled = false
 	current_mole = mole
 	current_mole.enabled = true
+
+func _on_mole_clicked(mole):
+
+	if mole == current_mole:
+		return
+	set_current_mole(mole)
