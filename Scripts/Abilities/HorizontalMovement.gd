d7556eaf617ab1b184e09895141aff1a38392484
Robert <bobbyras90@gmail.com>
2020-07-02 18:11:58 -0700

    Added a small amount of momentum to the moles

--- Scripts/Abilities/HorizontalMovement.gd
+++ Scripts/Abilities/HorizontalMovement.gd
@@ -1,6 +1,8 @@
 extends Node
 
 export(int) var move_speed = 12000
+export(int) var move_start_growth = 500
+export(int) var move_stop_growth = 500
 export(NodePath) var ground_checker_path
 export(NodePath) var animated_sprite_path
 export(AudioStream) var run_audio_stream
@@ -33,22 +35,33 @@ func _process(delta):
 	_parse_horizontal_input()
 
 func _parse_horizontal_input():
-	var horizontal = 0
+	var input_direction = 0
 	
 	if Input.is_action_pressed("move_left"):
 		if facing_right:
 			facing_right = false
 			parent.scale.x = -parent.scale.x
-		horizontal -= move_speed
+		input_direction -= 1
 	elif Input.is_action_pressed("move_right"):
 		if not facing_right:
 			facing_right = true
 			parent.scale.x = -parent.scale.x
-		horizontal += move_speed
+		input_direction += 1
+		
 	
-	parent.velocity.x = horizontal
+	if input_direction == 0:
+		var current_velocity_direction = sign(parent.velocity.x)
+		var deceleration = -current_velocity_direction * move_stop_growth
+		var calculated_velocity = parent.velocity.x + deceleration
+		var clamped_velocity = max(abs(calculated_velocity), 0)
+		parent.velocity.x = current_velocity_direction * clamped_velocity
+	else:
+		var acceleration = input_direction * move_start_growth
+		var calculated_velocity = parent.velocity.x + acceleration
+		var clamped_velocity = clamp(calculated_velocity, -move_speed, move_speed)
+		parent.velocity.x = clamped_velocity
 	
-	if ground_checker.is_colliding and horizontal != 0:
+	if ground_checker.is_colliding and input_direction != 0:
 		if not running:
 			$RunAudio.play()
 			animated_sprite.play("Run")
